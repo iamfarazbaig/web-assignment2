9558f78a327458a167ad8b7c3c59a738fd190569
Faraz <iamfarazbaig@gmail.com>
2019-05-18 18:24:06 +0100

    CRA added

--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,29 @@ const PostSchema = new Schema({
   date: {
     type: Date,
     default: Date.now()
-  }
+  },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now()
+      }
+    }
+  ]
 });
 
 module.exports = Post = mongoose.model("post", PostSchema);
