3f20464b42e959f85ee13ae76b3b102b7c83d1a9
Faraz <iamfarazbaig@gmail.com>
2019-05-20 06:12:55 +0100

    incomplete post

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,4 +2,5 @@ import alert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
 import { combineReducers } from "redux";
+import post from "./post";
 export default combineReducers({ alert, auth, profile });
